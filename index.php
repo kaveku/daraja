6fd3e6ac2f6a8b4fc5eb3c29f4eca6456c7623e4
kaveku <ligoangela.16@gmail.com>
2020-07-15 12:23:20 +0300

    news

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 echo "Yes I know!!!";
 ?>
+<html>
+<head></head>
+<body>
+<div onlick="myfucntion()">
+GOOO
+</div>
+</body>
+</html>
+<script>
+function myfunction(){
+    alert ("i know");
+}
+</script>

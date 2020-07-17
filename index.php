71b688c54abf6da195b77a857c3c316eed927fc0
kaveku <ligoangela.16@gmail.com>
2020-07-17 20:05:28 +0300

    initial commit

--- index.php
+++ index.php
@@ -1,16 +1,49 @@
-<?php
-echo "Yes I know!!!";
-?>
-<html>
-<head></head>
-<body>
-<button onlick="myfunction()">
-GOOO
-</button>
-</body>
-</html>
-<script type="text/javascript">
-function myfunction(){
-    alert ("i know");
-}
-</script>
+<!DOCTYPE html> 
+<head> 
+    <title> 
+        How to get selected value in  
+        dropdown list using JavaScript? 
+    </title> 
+</head> 
+  
+<body> 
+    <h1 style="color: green"> 
+        GeeksForGeeks 
+    </h1> 
+      
+    <b> 
+        How to get selected value in  
+        dropdown list using JavaScript? 
+    </b> 
+      
+    <p> 
+        Select one from the given options: 
+        <select id="select1"> 
+            <option value="free" id="k">Free</option> 
+            <option value="basic" id="m">Basic</option> 
+            <option value="premium" id='j'>Premium</option> 
+        </select> 
+    </p> 
+      
+    <p> 
+        The value of the option selected is:  
+        <span class="output"></span> 
+    </p> 
+      
+    <button onclick="getOption()"> 
+        Check option 
+    </button> 
+      
+    <script type="text/javascript">s 
+        function getOption() { 
+            selectElement = document.querySelector('#select1'); 
+                      
+            output =  
+              selectElement.options[selectElement.selectedIndex].value; 
+  alert(output);
+            //document.querySelector('.output').textContent = output; 
+        } 
+    </script> 
+</body> 
+  
+</html> 
